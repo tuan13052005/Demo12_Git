e3ba1d4cabe44ce844e477248436aa7e6730f86c
Pham Anh Tuan <phamanht7@gmail.com>
2024-02-23 19:37:57 +0700

    Print Hello

--- src/com/tn/Program.java
+++ src/com/tn/Program.java
@@ -3,5 +3,8 @@ package com.tn;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello git 2");
+        System.out.println("Hello git 3");
+
     }
 }
